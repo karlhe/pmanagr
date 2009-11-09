1079079f03e35ce770145c9eea450337643d46ae
karl.he <karl.he@bea25e74-c1a7-11de-91a4-794fdb80bb57>
2009-11-09 09:48:34 +0000

    Added details to specs

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -6,5 +6,34 @@ describe TasksController do
   it "should use TasksController" do
     controller.should be_an_instance_of(TasksController)
   end
-
+  
+  describe "When creating a task" do
+    it "should be belong to the current project"
+    it "should have a due by date after the current time"
+    it "should redirect to the task page"
+    it "should be shown on the project page"
+  end
+  
+  describe "When assigning a task" do
+    describe "When assigning yourself a task" do
+      it "should set the current user as a user for the task"
+      it "should add the task to the user's assignments"
+      it "should redirect to the task page"
+    end
+    describe "When assigning someone else a task as an admin" do
+      it "should set the target user as a user for the task"
+      it "should add the task to the target user's assignments"
+      it "should redirect to the task page"
+    end
+  end
+  
+  describe "When completing a task" do
+    it "should set the completed by time to the current time"
+  end
+  
+  describe "When deleting a task" do
+    it "should delete all subtasks"
+    it "should remove all assignments"
+  end
+  
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -10,4 +10,12 @@ describe Project do
   it "should create a new instance given valid attributes" do
     Project.create!(@valid_attributes)
   end
+  
+    it "should be set to either public or private"
+  
+  describe "Given valid attributes" do
+    it "should have a user with admin privileges in its memberships"
+    it "should have a valid name"
+    it "should have a valid description"
+  end
 end
