c7f25adb8c00de1d9f7a0cd2eeb21a14c797f611
karl.he <karl.he@bea25e74-c1a7-11de-91a4-794fdb80bb57>
2009-11-09 20:40:06 +0000

    some rspec tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   has_many :memberships
   has_many :users, :through => :memberships
   has_many :tasks, :dependent => :destroy
+  
+  validates_presence_of :name, :desc
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,28 +1,63 @@
 require 'spec_helper'
 
 describe ProjectsController do
+  fixtures :users, :projects
 
   #Delete this example and add some real ones
   it "should use ProjectsController" do
     controller.should be_an_instance_of(ProjectsController)
   end
   
-  describe "When creating a project" do
-    it "should create a project"
-    it "should set the current user to admin privileges"
-    it "should add the current user to the project"
-    it "should redirect to the project page"
+  describe "POST create" do
+    describe "if user logged in" do
+      before :each do
+        @current_user = users(:quentin)
+        controller.stub!(:current_user).and_return(@current_user)
+        controller.stub!(:login_required).and_return(:true)
+        @project = projects(:newprojectn)
+      end
+      it "should create a project" do
+        Project.stub!(:new).and_return @project
+        Project.should_receive(:new)
+        @project.should_receive(:save)
+        post :create
+      end
+      it "should set the current user to admin privileges"
+      it "should add the current user to the project" do
+        Project.stub!(:new).and_return @project
+        post :create
+        response.should redirect_to(join_project_path(@project))
+      end
+      it "should redirect to the project page"
+    end
   end
   
-  describe "When joining a project" do
-    it "should add current user as a member"
-    it "should give the current user minimum privileges"
-    it "should redirect to the project page"
+  describe "POST join" do
+    describe "if user logged in" do
+      before :each do
+        @current_user = users(:quentin)
+        controller.stub!(:current_user).and_return(@current_user)
+        controller.stub!(:login_required).and_return(:true)
+        @project = projects(:projectn)
+      end
+      it "should add current user as a member" do
+        Project.stub!(:find).and_return @project
+        post :join
+        @project.users.include?(@current_user).should == true
+      end
+      it "should give the current user minimum privileges"
+      it "should redirect to the project page"
+    end
   end
   
-  describe "When showing projects" do
+  describe "GET index" do
+    it "should display a list of projects" do
+      @projects = [projects(:projectp), projects(:projectq)]
+      Project.should_receive(:find).and_return @projects
+      get :index
+    end
     describe "If the project is public" do
-      it "should be list"
+      it "should be listed"
       it "should display to anyone"
     end
     describe "If the project is private" do
