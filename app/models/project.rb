d4e619ba18a2788a8397ad311489905650efc82e
Anhang <Anhang@bea25e74-c1a7-11de-91a4-794fdb80bb57>
2009-11-21 00:41:55 +0000

    schema: projects hm tasks hm assignments

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,6 @@ class Project < ActiveRecord::Base
   has_many :memberships
   has_many :users, :through => :memberships
   has_many :tasks, :dependent => :destroy
-  has_many :assignments, :dependent => :destroy
   has_many :threads, :dependent => :destroy
   
   validates_presence_of :name, :desc
