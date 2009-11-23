c4fef7a1d04136165314b211610f9e87c9639fd4
Anhang <Anhang@bea25e74-c1a7-11de-91a4-794fdb80bb57>
2009-11-23 21:00:06 +0000

    Fixed attr accessible bug

--- trunk/app/models/project.rb
+++ trunk/app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
   
   validates_presence_of :name, :desc
 
-  attr_accessible :start_time
+  attr_accessible :start_time, :name, :desc, :public, :due_by, :completed_at, :created_at, :updated_at
   
   def is_public?
     return public
