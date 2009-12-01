74de3b68ab13eda9c0ea15a29d4e24aca90f9ab1
anhang <anhang@bea25e74-c1a7-11de-91a4-794fdb80bb57>
2009-12-01 09:35:44 +0000

    user/edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,23 @@ class ApplicationController < ActionController::Base
   
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  private
+  
+  def check_public_access
+    private = !Project.find(params[:id]).is_public?
+    if (!logged_in? and private)  || (logged_in? and private and (!current_user.memberships.select{|m| m.project_id.to_s == params[:id]}.first))
+      redirect_to root_path
+      flash[:error] = 'You do not have permission to view this private project.'
+    end
+  end
+
+  def check_admin
+    project_admin = Project.find(params[:id]).admin
+    #assignment_admin = Assignment.find(params[:assignment_id]).admin
+    if (project_admin != current_user)
+      redirect_to root_path
+      flash[:error] = 'You do not have permission to destroy this project.'
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,4 +38,30 @@ class UsersController < ApplicationController
       render :action => 'new'
     end
   end
+
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+ 
+  end
+
+  # PUT /users/1
+  # PUT /users/1.xml
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to root_path }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+
 end
